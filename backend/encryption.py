0442a0818306a11b76d2b28b3c5f67fc06792a56
ANJALI-RWT <anjalirwt925@outlook.com>
2025-06-17 13:58:39 +0530

    Update encryption.py

--- backend/encryption.py
+++ backend/encryption.py
@@ -2,6 +2,31 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 import base64
 import os
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import base64
+
+class RSAEncryption:
+    @staticmethod
+    def generate_keys():
+        key = RSA.generate(2048)
+        private_key = key.export_key().decode()
+        public_key = key.publickey().export_key().decode()
+        return public_key, private_key
+
+    @staticmethod
+    def encrypt_with_public_key(message, public_key_str):
+        public_key = RSA.import_key(public_key_str.encode())
+        cipher = PKCS1_OAEP.new(public_key)
+        encrypted = cipher.encrypt(message.encode())
+        return base64.b64encode(encrypted).decode()
+
+    @staticmethod
+    def decrypt_with_private_key(encrypted_message, private_key_str):
+        private_key = RSA.import_key(private_key_str.encode())
+        cipher = PKCS1_OAEP.new(private_key)
+        decrypted = cipher.decrypt(base64.b64decode(encrypted_message))
+        return decrypted.decode()
 
 class AesEncryption:
     @staticmethod
